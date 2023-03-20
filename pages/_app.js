262455225af48c67d48b33678914832be7f6883e
unknown <argene_tanyong@yahoo.com>
2023-03-20 18:27:29 +0800

    IMplemented get request using React Query and displaying of data.

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,17 @@
 import Layout from "../components/Layout";
 import { MantineProvider } from "@mantine/core";
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }) {
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <QueryClientProvider client={queryClient}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </QueryClientProvider>
     </MantineProvider>
   );
 }
